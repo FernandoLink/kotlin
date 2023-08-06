442266ce64e97da042ac5830ee8b3b08ebe88ee3
Fernando Link <fernandolink@gmail.com>
2023-08-06 11:21:51 -0300

    equals hashCode toString

--- bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -6,7 +6,8 @@ import br.com.alura.bytebank.teste.testaObjects
 
 fun main() {
 
-    val endereco = Endereco(logradouro = "Rua Vergueiro", complemento = "Alura")
+    val endereco = Endereco(logradouro = "Rua Dom Joao Vi", complemento = "Sobrado 4", cep = "82900-150")
+    val enderecoNovo = Endereco(logradouro = "Rua Dom Joao Vi", complemento = "Sobrado 4", cep = "82900-150")
 
     imprime(1)
     imprime(1.0)
@@ -20,6 +21,11 @@ fun main() {
     testaFuncionarios()
 
     println("${endereco.javaClass}@${Integer.toHexString(endereco.hashCode())}")
+
+    println(endereco.equals(enderecoNovo))
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
+
 
 }
 
