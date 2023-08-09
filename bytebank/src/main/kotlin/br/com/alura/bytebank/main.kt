42c9877ce00978cfcdc8cd791a7c33d575b717f7
Fernando Link <fernandolink@gmail.com>
2023-08-08 21:03:47 -0300

    safety null

--- bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,33 +1,13 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.exception.SaldoInsuficienteException
 import br.com.alura.bytebank.modelo.Endereco
-import br.com.alura.bytebank.teste.testeExpressao
 
 fun main() {
-    println("início main")
-    funcao1()
-    println("fim main")
+    var enderecoNulo: Endereco? = null
+    println(enderecoNulo?.logradouro?.length)
 }
 
-fun funcao1(){
-    println("início funcao1")
-    try {
-        funcao2()
-    } catch(e: SaldoInsuficienteException){
-        println("Saldo Insuficiente Exception")
-    }
-    println("fin funcao1")
-}
 
-fun funcao2(){
-    println("início funcao2")
-    for (i in 1..5){
-        println(i)
-        throw SaldoInsuficienteException()
-    }
-    println("fim funcao2")
-}
 
 
 
