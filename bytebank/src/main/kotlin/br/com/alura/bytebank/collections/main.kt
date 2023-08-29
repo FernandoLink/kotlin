e936cea6fa01b7b93629033be7b772556d156bbf
Fernando Link <fernandolink@gmail.com>
2023-08-29 17:58:39 -0300

    paradigma funcional

--- bytebank/src/main/kotlin/br/com/alura/bytebank/collections/main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/collections/main.kt
@@ -4,6 +4,39 @@ import java.lang.NullPointerException
 import br.com.alura.bytebank.collections.testaSet
 
 fun main() {
-    testaAssociateMap()
+    println(soma(1,5))
+
+    val minhaFuncao = ::soma2
+    println(minhaFuncao(1, 2))
+
+    val minhaFuncaoClasse = Teste()
+    println(minhaFuncaoClasse())
+
+    val teste = Teste()
+    teste(10)
+
+    val minhaFuncaoLambda= {
+        println("Executa com lambada")
+    }
+    println(minhaFuncaoLambda())
+
+    val minhaFuncaoAnonima: () -> Unit = fun(){
+        println("Executa como anonima")
+    }
+    println(minhaFuncaoAnonima())
+}
+fun soma(a: Int, b: Int) : Int = a + b
+
+fun soma2(a: Int, b: Int): Int{
+    return a + b
 }
 
+class Teste : () -> Unit {
+    override fun invoke() {
+        println("executa invoke do teste")
+    }
+    operator fun invoke(valor: Int){
+        println(valor)
+    }
+
+}
