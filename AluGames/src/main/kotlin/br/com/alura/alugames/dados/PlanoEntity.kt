174811473fb64d759f450d807fe53aaa7b086386
Fernando Link <fernandolink@gmail.com>
2023-09-27 18:24:06 -0300

    gamer a plano

--- AluGames/src/main/kotlin/br/com/alura/alugames/dados/PlanoEntity.kt
+++ AluGames/src/main/kotlin/br/com/alura/alugames/dados/PlanoEntity.kt
@@ -1,15 +1,24 @@
 package br.com.alura.alugames.dados
 
-import javax.persistence.*
+import javax.persistence.DiscriminatorColumn
+import javax.persistence.DiscriminatorType
+import javax.persistence.DiscriminatorValue
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Inheritance
+import javax.persistence.InheritanceType
+import javax.persistence.Table
+
 
 @Entity
-@Table(name="planos")
+@Table(name = "planos")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="TipoPlano", discriminatorType = DiscriminatorType.STRING)
+@DiscriminatorColumn(name = "TipoPlano", discriminatorType = DiscriminatorType.STRING)
 sealed class PlanoEntity(
     val tipo: String,
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int = 0)
 
 @Entity
@@ -23,5 +32,4 @@ class PlanoAssinaturaEntity(
     val mensalidade: Double = 0.0,
     val jogosIncluidos: Int = 0,
     val percentualDescontoReputacao: Double = 0.0,
-    id: Int = 0
-): PlanoEntity(tipo, id)
+    id: Int = 0): PlanoEntity(tipo, id)
