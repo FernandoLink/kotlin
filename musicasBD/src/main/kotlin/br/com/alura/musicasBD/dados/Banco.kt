46e88af71b1f6b72e9df30a593512731f427dd29
Fernando Link <fernandolink@gmail.com>
2023-09-28 14:31:28 -0300

    musicasBD

---
+++ musicasBD/src/main/kotlin/br/com/alura/musicasBD/dados/Banco.kt
@@ -0,0 +1,12 @@
+package br.com.alura.musicasBD.dados
+
+import javax.persistence.EntityManager
+import javax.persistence.Persistence
+
+
+object Banco {
+    fun getEntityManager(): EntityManager {
+        val factory = Persistence.createEntityManagerFactory("musicasBD")
+        return factory.createEntityManager()
+    }
+}
