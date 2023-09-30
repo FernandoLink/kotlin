74035216a2ccd429f7faaeae87a8b539e3b20dad
Fernando Link <fernandolink@gmail.com>
2023-09-30 16:58:50 -0300

    Kotlin Spring

---
+++ forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -0,0 +1,33 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.dto.NovoTopicoDto
+import br.com.alura.forum.model.Topico
+import org.springframework.stereotype.Service
+
+@Service
+class TopicoService(
+    private var topicos: List<Topico> = ArrayList(),
+    private val cursoService: CursoService,
+    private val usuarioService: UsuarioService
+) {
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+    }
+
+    fun cadastrar(dto: NovoTopicoDto) {
+        topicos = topicos.plus(Topico(
+            id = topicos.size.toLong() + 1,
+            titulo = dto.titulo,
+            mensagem = dto.mensagem,
+            curso = cursoService.buscarPorId(dto.idCurso),
+            autor = usuarioService.buscarPorId(dto.idAutor)
+        ))
+    }
+}
